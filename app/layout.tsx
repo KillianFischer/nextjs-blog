a6f47c5bf87eb01899d410330aa30ac2a55569f9
KillianFischer <killian.fischer1@gmail.com>
2025-01-21 21:00:10 +0100

    Add ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
       <head>
         <Script
           async
-          src={`https://pagead2.googlesyndleware.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_ADSENSE_CLIENT_ID}`}
+          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_ADSENSE_CLIENT_ID}`}
           crossOrigin="anonymous"
           strategy="afterInteractive"
         />
